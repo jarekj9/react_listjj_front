716e4d2f71211fc69d22c2e6eefafedb9a4a1211
Jaroslaw <jaroslaw.jankun@gmail.com>
2023-11-30 19:20:00 +0100

    Style login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -19,14 +19,35 @@ const handleLogin = async () => {
 };
 
   return (
-    <div>
-      <label>Email:</label>
-      <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
-
-      <label>Password:</label>
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-
-      <button onClick={handleLogin}>Login</button>
+    <div class="container col-sm-12 col-md-10 col-lg-6 col-xl-4">
+      <div class="card">  
+        <div class="card-header">
+          <h3>ListJJ</h3>
+        </div>    
+      <div class="card-body">
+          <div class="d-flex flex-row justify-content-center">
+            <div class="d-flex flex-column justify-content-center">
+              <div class="p-2 align-self-start">
+                <label>Email:</label>
+              </div>
+              <div class="p-2 align-self-start">
+                <label>Password:</label>
+              </div>
+            </div>
+            <div class="d-flex flex-column">
+            <div class="p-2">
+                <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+              </div>
+              <div class="p-2">
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+              </div>
+            </div>
+          </div>
+          <div class="d-flex flex-row justify-content-center">
+            <button class="btn btn-secondary mt-3" onClick={handleLogin}>Login</button>
+          </div>
+        </div>  
+      </div>
     </div>
   );
 };
